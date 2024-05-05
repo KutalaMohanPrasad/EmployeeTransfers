8f12f1632177687614b6545026fdfb6189206d9e
Kutala Mohan Prasad <77629263+KutalaMohanPrasad@users.noreply.github.com>
2024-05-05 16:57:44 -0400

    Add files via upload

---
+++ src/main/java/com/employee/Transfers/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.employee.Transfers;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(EmployeeTransfersApplication.class);
+	}
+
+}
---
+++ src/test/java/com/employee/Transfers/EmployeeTransfersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employee.Transfers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeTransfersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
